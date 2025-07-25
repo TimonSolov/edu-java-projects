8fb2af6a4ca9b3c742bfc5c0e8b95f327920edc4
TimonSolov <tim.colok@gmail.com>
2025-07-25 21:07:43 +0200

    added task1

---
+++ edu-java-homework-task-1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//        Вывести последнюю цифру числа
+//        Найти сумму цифр ab + cd числа abcd
+//        Вывести 3юю цифру 5ти-значного числа
+//        Вывести длину отрезка, проходящей через две точки
+        int given_number = 56432;
+        int last_digit = given_number % 10;
+        System.out.println("The given number is: " + given_number);
+        System.out.println("The last digit is: " + last_digit);
+
+    }
+}
